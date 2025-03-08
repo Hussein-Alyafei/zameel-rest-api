4b48284e7f19d37bc163ad4d60c3cf55f09c004f
Hussein-Alyafei <hussein.alyafei2002@gmail.com>
2025-03-08 19:35:27 +0300

    Remove subject relationship endpoints

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,6 @@ use App\Http\Controllers\CollegeController;
 use App\Http\Controllers\CollegeMajorController;
 use App\Http\Controllers\MajorCollegeController;
 use App\Http\Controllers\MajorController;
-use App\Http\Controllers\SubjectAssignmentController;
-use App\Http\Controllers\SubjectBookController;
 use App\Http\Controllers\SubjectController;
 use Illuminate\Support\Facades\Route;
 use Orion\Facades\Orion;
@@ -23,6 +21,4 @@ Route::middleware('auth:sanctum')->group(function () {
     Orion::belongsToResource('majors', 'college', MajorCollegeController::class)->withSoftDeletes();
 
     Orion::resource('subjects', SubjectController::class)->withSoftDeletes();
-    Orion::hasManyResource('subjects', 'assignments', SubjectAssignmentController::class);
-    Orion::hasManyResource('subjects', 'books', SubjectBookController::class);
 });
