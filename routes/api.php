7e580613766c8e38a3c427a77c22ae6152df95a6
Hussein Alyafei <111661881+Hussein-Alyafei@users.noreply.github.com>
2025-03-11 22:51:54 +0300

    Add group endpoints (#20)

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,12 @@ use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use App\Http\Controllers\CollegeController;
-use App\Http\Controllers\CollegeMajorController;
+use App\Http\Controllers\CollegeMajorsController;
+use App\Http\Controllers\GroupController;
+use App\Http\Controllers\GroupMajorController;
 use App\Http\Controllers\MajorCollegeController;
 use App\Http\Controllers\MajorController;
+use App\Http\Controllers\MajorGroupsController;
 use App\Http\Controllers\SubjectController;
 use Illuminate\Support\Facades\Route;
 use Orion\Facades\Orion;
@@ -19,12 +22,16 @@ Route::post('/logout', [AuthenticationController::class, 'logout'])->middleware(
 
 Route::middleware('auth:sanctum')->group(function () {
     Orion::resource('colleges', CollegeController::class)->withSoftDeletes();
-    Orion::hasManyResource('colleges', 'majors', CollegeMajorController::class)->withSoftDeletes();
+    Orion::hasManyResource('colleges', 'majors', CollegeMajorsController::class)->withSoftDeletes();
 
     Orion::resource('majors', MajorController::class)->withSoftDeletes();
     Orion::belongsToResource('majors', 'college', MajorCollegeController::class)->withSoftDeletes();
+    Orion::hasManyResource('majors', 'groups', MajorGroupsController::class)->except(GroupController::EXCLUDE_METHODS);
 
     Orion::resource('subjects', SubjectController::class)->withSoftDeletes();
+
+    Orion::resource('groups', GroupController::class)->except(GroupController::EXCLUDE_METHODS);
+    Orion::belongsToResource('groups', 'major', GroupMajorController::class)->withSoftDeletes();
 
     Route::post('verify-email', VerifyEmailController::class)
         ->middleware(['throttle:6,1'])
